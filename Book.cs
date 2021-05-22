5a4744a7dd1a0bb7ad5640f3d2d8fca43750e06f
Amanda Hayes <hayesmdesign@gmail.com>
2021-05-22 10:08:31 -0700

    created book class and book objects

---
+++ Book.cs
@@ -0,0 +1,16 @@
+namespace democonsole
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+        public Book(string aTitle, string aAuthor, int aPages)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace democonsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Harry Potter", "JK Rowling", 400);
+            Book book2 = new Book("Lord of the Rings", "J.R.R Tolkein", 700);
+
+            Console.WriteLine(book1.pages);
+            Console.ReadLine();
+        }
+    }
+}
